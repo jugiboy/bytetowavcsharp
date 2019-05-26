37a0b160dba4aaa6c3b4c24f3a6445dacb51067f
Jukka Rajaniemi <jukkar123@gmail.com>
2019-05-26 13:01:58 +0300

    Add project files.

---
+++ converter/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace converter
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        /* READ!!!
+         * How to use?
+         * Get the bytes of a sound file you want to convert to a wav
+         * Open sound_bytes.cs and paste them in
+         * Build and press convert
+         * Done
+        */
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            sound_bytes soundBytes = new sound_bytes(); // initialize our sound_bytes class
+            var bytes = soundBytes.bytes_sound; // get the bytes
+            using (FileStream fs = File.Create("converted.wav")) // create your wav file
+            {
+                fs.Write(bytes, 0, bytes.Length); // write the bytes into the wav file
+            }
+            Thread.Sleep(250); // sleep for 250ms just so it looks nicer lol
+            MessageBox.Show("Successfully converted to WAV file!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information); // done .wav file is in the same folder as this .exe
+        }
+    }
+}
