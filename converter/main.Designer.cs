ec243a12ab4f8addcbbe52a4e073a6d2ff3edaf5
Jukka Rajaniemi <jukkar123@gmail.com>
2020-01-15 20:25:57 +0200

    added drag and drop function, todo: make the convert button work again

--- converter/main.Designer.cs
+++ converter/main.Designer.cs
@@ -29,37 +29,95 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtBytes = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rbCpp = new System.Windows.Forms.RadioButton();
+            this.rbCSharp = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(52, 27);
+            this.button1.Location = new System.Drawing.Point(12, 191);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 57);
+            this.button1.Size = new System.Drawing.Size(72, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // Form1
+            // txtBytes
             // 
+            this.txtBytes.AllowDrop = true;
+            this.txtBytes.Location = new System.Drawing.Point(12, 12);
+            this.txtBytes.Multiline = true;
+            this.txtBytes.Name = "txtBytes";
+            this.txtBytes.Size = new System.Drawing.Size(313, 173);
+            this.txtBytes.TabIndex = 1;
+            this.txtBytes.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.txtBytes.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Play Sound";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rbCpp
+            // 
+            this.rbCpp.AutoSize = true;
+            this.rbCpp.Location = new System.Drawing.Point(331, 13);
+            this.rbCpp.Name = "rbCpp";
+            this.rbCpp.Size = new System.Drawing.Size(82, 17);
+            this.rbCpp.TabIndex = 3;
+            this.rbCpp.TabStop = true;
+            this.rbCpp.Text = "Print as C++";
+            this.rbCpp.UseVisualStyleBackColor = true;
+            // 
+            // rbCSharp
+            // 
+            this.rbCSharp.AutoSize = true;
+            this.rbCSharp.Location = new System.Drawing.Point(331, 36);
+            this.rbCSharp.Name = "rbCSharp";
+            this.rbCSharp.Size = new System.Drawing.Size(77, 17);
+            this.rbCSharp.TabIndex = 3;
+            this.rbCSharp.TabStop = true;
+            this.rbCSharp.Text = "Print as C#";
+            this.rbCSharp.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 111);
+            this.ClientSize = new System.Drawing.Size(419, 229);
+            this.Controls.Add(this.rbCSharp);
+            this.Controls.Add(this.rbCpp);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBytes);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "main";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Convert bytes to WAV";
+            this.Load += new System.EventHandler(this.main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBytes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton rbCpp;
+        private System.Windows.Forms.RadioButton rbCSharp;
     }
 }
 
--- converter/main.cs
+++ converter/main.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Media;
+using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -19,17 +20,101 @@ namespace converter
          * Build and press convert
          * Done
         */
+        byte[] getbytearray = { 0x00 };
+        
         private void Button1_Click(object sender, EventArgs e)
         {
-            
-            sound_bytes soundBytes = new sound_bytes(); // initialize our sound_bytes class
-            var bytes = soundBytes.bytes_sound; // get the bytes
             using (FileStream fs = File.Create("converted.wav")) // create your wav file
             {
-                fs.Write(bytes, 0, bytes.Length); // write the bytes into the wav file
+                if (getbytearray != null)
+                {
+                    fs.Write(getbytearray, 0, getbytearray.Length); // write the bytes into the wav file
+                }
+                fs.Close();
             }
-            Thread.Sleep(250); // sleep for 250ms just so it looks nicer lol
             MessageBox.Show("Successfully converted to WAV file!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information); // done .wav file is in the same folder as this .exe
+            
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] get_dropped = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach(string file in get_dropped)
+            {
+                if(!file.Contains(".wav"))
+                {
+                    MessageBox.Show("Only .wav files are allowed");
+                    return;
+                }
+                var read_bytes = File.ReadAllBytes(file);
+                getbytearray = read_bytes;
+                if (rbCpp.Checked)
+                {
+                    txtBytes.Text = PrintByteArrayCPP(read_bytes);
+                }
+                else if (rbCSharp.Checked)
+                {
+                    txtBytes.Text = PrintByteArrayCSharp(read_bytes);
+                }
+                else if (!rbCpp.Checked && !rbCSharp.Checked)
+                {
+                    txtBytes.Text = PrintByteArrayNone(read_bytes);
+                }
+            }
+        }
+        public string PrintByteArrayNone(byte[] bytes)
+        {
+            var sb = new StringBuilder("");
+            foreach (var b in bytes)
+            {
+                sb.Append("0x" + b + ", ");
+            }
+            return sb.ToString();
+        }
+        public string PrintByteArrayCPP(byte[] bytes)
+        {
+            var sb = new StringBuilder("unsigned char[] = {");
+            foreach (var b in bytes)
+            {
+                sb.Append("0x" + b + ", ");
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+        public string PrintByteArrayCSharp(byte[] bytes)
+        {
+            var sb = new StringBuilder("byte[] sound_bytes = {");
+            foreach (var b in bytes)
+            {
+                sb.Append("0x" + b + ", ");
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) 
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer pl = new SoundPlayer();
+            pl.SoundLocation = Application.StartupPath + "\\converted.wav";
+            try
+            {
+                pl.Play();
+            }
+            catch
+            {
+                MessageBox.Show("No converted audio file found!");
+            }
+            pl.Dispose();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
